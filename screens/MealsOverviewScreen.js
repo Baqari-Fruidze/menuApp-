f889c469e81eb599ba4b3f2c72f3854259ce6f1f
Baqari-Fruidze <baqarifruidze@gmail.com>
2025-03-25 17:21:46 +0400

    single items

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,22 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 function MealsOverviewScreen({ route }) {
   const catId = route.params.categoryId;
+  const displayedMeals = MEALS.filter(
+    (mealItem) => mealItem.categoryIds.indexOf(catId) >= 0
+  );
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
   return (
     <View style={styles.container}>
-      <Text>meals overview screen -- {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
