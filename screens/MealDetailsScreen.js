8d030647bc5200abe2cd323867888c36e2e671a8
Baqari-Fruidze <154324770+Baqari-Fruidze@users.noreply.github.com>
2025-04-14 00:30:20 +0400

    navigation

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,7 +1,11 @@
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { Text } from "react-native";
 
-function MealDetailsScreen() {
-  return <Text>meals details</Text>;
+function MealDetailsScreen({ route }) {
+  // const route = useRoute();
+  const { mealId } = route.params;
+
+  return <Text>meals details ({mealId})</Text>;
 }
 
 export default MealDetailsScreen;
